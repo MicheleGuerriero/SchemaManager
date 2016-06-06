0ca34de2f4c5502349c8d57c6a4956eee68253e8
micheleguerriero <michele.guerriero2391@gmail.com>
2016-06-06 19:23:28 +0200

    Initial commit.

---
+++ src/main/java/it/polimi/schema_manager/App.java
@@ -0,0 +1,13 @@
+package it.polimi.schema_manager;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
